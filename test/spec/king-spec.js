71d19f88a36ad71abd82d71fea4169425d088e00
Soraismus <tertius_lupus@yahoo.com>
2014-05-13 19:53:50 -0400

    Redo spec for k.getPotentialKingAttacks.

--- test/spec/king-spec.js
+++ test/spec/king-spec.js
@@ -4,7 +4,7 @@
     var contravenes, descr, each_hyphen_contravenes, each_hyphen_fn_hyphen_result_hyphen_equals, each_hyphen_satisfies, each_hyphen_throws, gen_hyphen_pred_hyphen_guards, satisfies, t, test_hyphen_predicate, test_hyphen_spec, test_hyphen_throwing_hyphen_transformation, test_hyphen_transformation, throws, to_hyphen_be, to_hyphen_be_hyphen_array, to_hyphen_be_hyphen_false, to_hyphen_be_hyphen_fn, to_hyphen_be_hyphen_nbr, to_hyphen_be_hyphen_true, to_hyphen_equal, to_hyphen_have_hyphen_length, to_hyphen_throw, xdescr, xt;
     descr = j.descr, xdescr = j.xdescr, t = j.t, xt = j.xt, test_hyphen_spec = j.test_hyphen_spec, to_hyphen_throw = j.to_hyphen_throw, to_hyphen_be = j.to_hyphen_be, to_hyphen_equal = j.to_hyphen_equal, to_hyphen_be_hyphen_true = j.to_hyphen_be_hyphen_true, to_hyphen_be_hyphen_false = j.to_hyphen_be_hyphen_false, satisfies = j.satisfies, contravenes = j.contravenes, throws = j.throws, each_hyphen_fn_hyphen_result_hyphen_equals = j.each_hyphen_fn_hyphen_result_hyphen_equals, each_hyphen_satisfies = j.each_hyphen_satisfies, each_hyphen_contravenes = j.each_hyphen_contravenes, each_hyphen_throws = j.each_hyphen_throws, to_hyphen_be_hyphen_array = j.to_hyphen_be_hyphen_array, to_hyphen_be_hyphen_fn = j.to_hyphen_be_hyphen_fn, to_hyphen_be_hyphen_nbr = j.to_hyphen_be_hyphen_nbr, to_hyphen_be_hyphen_array = j.to_hyphen_be_hyphen_array, to_hyphen_have_hyphen_length = j.to_hyphen_have_hyphen_length, test_hyphen_predicate = j.test_hyphen_predicate, test_hyphen_transformation = j.test_hyphen_transformation, test_hyphen_throwing_hyphen_transformation = j.test_hyphen_throwing_hyphen_transformation, gen_hyphen_pred_hyphen_guards = j.gen_hyphen_pred_hyphen_guards;
     return descr('test king.cough')(function() {
-      var bd, bd3, bd4, chart1, chart2, chart2_1, ctx, factory, initial_hyphen_board, invalid_hyphen_board, invalid_hyphen_chessman, invalid_hyphen_pos, map, passant, path, pos1, pos2, pos3, rts, setting, src, tgt;
+      var bd, bd3, bd4, chart1, chart2, chart2_1, ctx, factory, gen_hyphen_pos, initial_hyphen_board, invalid_hyphen_board, invalid_hyphen_chessman, invalid_hyphen_pos, map, passant, path, pos1, pos2, pos3, rts, setting, src, tgt;
       bd = sc.boards[0];
       chart1 = sc.charts[0];
       chart2 = sc.charts[1];
@@ -23,6 +23,7 @@
       setting = sc.settings[0];
       src = sc.positions[0];
       tgt = sc.positions[1];
+      gen_hyphen_pos = g.generate_hyphen_position;
       factory = function(src) {
         return function(bd) {
           return g.generate_hyphen_chart2({
@@ -37,9 +38,10 @@
       bd4 = '----kb-----Np---------------------------------------------------';
       pos3 = g.generate_hyphen_position(0, 4);
       t('test k.getPotentialKingAttacks')(function() {
-        var fn;
+        var fn, res;
         fn = k.getPotentialKingAttacks;
-        return to_hyphen_equal([])(fn(bd)(pos1));
+        res = [gen_hyphen_pos(1, 5), gen_hyphen_pos(1, 3), gen_hyphen_pos(0, 3)];
+        return to_hyphen_equal(res)(fn(bd4)(gen_hyphen_pos(0, 4)));
       });
       t('test k.getPotentialMoves')(function() {
         var chart3, chart4, chart5, fn, gen_hyphen_chart1, results3, results4, results5;
