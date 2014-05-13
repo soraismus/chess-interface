8543275f692cc973ada0735ed9924e00cd8f0354
Soraismus <tertius_lupus@yahoo.com>
2014-05-13 16:00:19 -0400

    Add spec for the fn #king-position.

--- test/spec/game-service-spec.js
+++ test/spec/game-service-spec.js
@@ -4,7 +4,7 @@
     var contravenes, descr, each_hyphen_contravenes, each_hyphen_fn_hyphen_result_hyphen_equals, each_hyphen_satisfies, each_hyphen_throws, gen_hyphen_pred_hyphen_guards, satisfies, t, test_hyphen_predicate, test_hyphen_spec, test_hyphen_throwing_hyphen_transformation, test_hyphen_transformation, throws, to_hyphen_be, to_hyphen_be_hyphen_array, to_hyphen_be_hyphen_false, to_hyphen_be_hyphen_fn, to_hyphen_be_hyphen_nbr, to_hyphen_be_hyphen_true, to_hyphen_equal, to_hyphen_have_hyphen_length, to_hyphen_throw, xdescr, xt;
     descr = j.descr, xdescr = j.xdescr, t = j.t, xt = j.xt, test_hyphen_spec = j.test_hyphen_spec, to_hyphen_throw = j.to_hyphen_throw, to_hyphen_be = j.to_hyphen_be, to_hyphen_equal = j.to_hyphen_equal, to_hyphen_be_hyphen_true = j.to_hyphen_be_hyphen_true, to_hyphen_be_hyphen_false = j.to_hyphen_be_hyphen_false, satisfies = j.satisfies, contravenes = j.contravenes, throws = j.throws, each_hyphen_fn_hyphen_result_hyphen_equals = j.each_hyphen_fn_hyphen_result_hyphen_equals, each_hyphen_satisfies = j.each_hyphen_satisfies, each_hyphen_contravenes = j.each_hyphen_contravenes, each_hyphen_throws = j.each_hyphen_throws, to_hyphen_be_hyphen_array = j.to_hyphen_be_hyphen_array, to_hyphen_be_hyphen_fn = j.to_hyphen_be_hyphen_fn, to_hyphen_be_hyphen_nbr = j.to_hyphen_be_hyphen_nbr, to_hyphen_be_hyphen_array = j.to_hyphen_be_hyphen_array, to_hyphen_have_hyphen_length = j.to_hyphen_have_hyphen_length, test_hyphen_predicate = j.test_hyphen_predicate, test_hyphen_transformation = j.test_hyphen_transformation, test_hyphen_throwing_hyphen_transformation = j.test_hyphen_throwing_hyphen_transformation, gen_hyphen_pred_hyphen_guards = j.gen_hyphen_pred_hyphen_guards;
     return descr('test game-service.cough')(function() {
-      var Kkq, bd1, bd10, bd11, bd12, bd2, bd3, bd4, bd5, bd6, bd7, bd8, bd9, chart1, chart4, chart5, gen_hyphen_chart, gen_hyphen_chart2, gen_hyphen_pos, pos0_hyphen_0, pos0_hyphen_3, pos0_hyphen_4, pos1_hyphen_4, pos2_hyphen_3, pos2_hyphen_4, pos2_hyphen_6, pos3_hyphen_4, pos5_hyphen_1, pos5_hyphen_2, pos7_hyphen_1, pos7_hyphen_3, pos7_hyphen_4;
+      var Kkq, bd1, bd10, bd11, bd12, bd2, bd3, bd4, bd5, bd6, bd7, bd8, bd9, chart1, chart4, chart5, gen_hyphen_chart, gen_hyphen_chart2, gen_hyphen_pos, pos0_hyphen_0, pos0_hyphen_3, pos0_hyphen_4, pos1_hyphen_4, pos2_hyphen_3, pos2_hyphen_4, pos2_hyphen_6, pos3_hyphen_4, pos5_hyphen_1, pos5_hyphen_2, pos7_hyphen_1, pos7_hyphen_3, pos7_hyphen_4, pos7_hyphen_6;
       gen_hyphen_pos = g.generate_hyphen_position;
       gen_hyphen_chart2 = g.generate_hyphen_chart2;
       bd1 = sc.initial_hyphen_board;
@@ -32,6 +32,7 @@
       pos7_hyphen_1 = gen_hyphen_pos(7, 1);
       pos7_hyphen_3 = gen_hyphen_pos(7, 3);
       pos7_hyphen_4 = gen_hyphen_pos(7, 4);
+      pos7_hyphen_6 = gen_hyphen_pos(7, 6);
       Kkq = 'Kkq';
       gen_hyphen_chart = function(board, pos) {
         return gen_hyphen_chart2({
@@ -63,6 +64,13 @@
         res1 = [gen_hyphen_pos(1, 4), gen_hyphen_pos(2, 5), gen_hyphen_pos(3, 6), gen_hyphen_pos(4, 7), gen_hyphen_pos(1, 3), gen_hyphen_pos(2, 3), gen_hyphen_pos(3, 3), gen_hyphen_pos(4, 3), gen_hyphen_pos(5, 3), gen_hyphen_pos(6, 3), gen_hyphen_pos(7, 3), gen_hyphen_pos(1, 2), gen_hyphen_pos(2, 1), gen_hyphen_pos(3, 0), gen_hyphen_pos(0, 4), gen_hyphen_pos(0, 5), gen_hyphen_pos(0, 6), gen_hyphen_pos(0, 7), gen_hyphen_pos(0, 2), gen_hyphen_pos(0, 1), gen_hyphen_pos(0, 0)];
         return to_hyphen_equal(res1)(fn(c.queen, chart4));
       });
+      t('test gs.king-position')(function() {
+        var fn;
+        fn = gs.king_hyphen_position;
+        to_hyphen_equal(pos0_hyphen_4)(fn(c.black)(bd3));
+        to_hyphen_equal(pos7_hyphen_4)(fn(c.white)(bd3));
+        return to_hyphen_equal(pos7_hyphen_6)(fn(c.white)(bd9));
+      });
       t('test gs.modifyCastlingRights')(function() {
         var chart6, chart7, fn;
         fn = gs.modifyCastlingRights;
@@ -89,7 +97,7 @@
         to_hyphen_equal(bd7)(fn(chart11)(gen_hyphen_pos(0, 6)));
         chart12 = gen_hyphen_chart(bd5, pos7_hyphen_4);
         to_hyphen_equal(bd8)(fn(chart12)(gen_hyphen_pos(7, 2)));
-        to_hyphen_equal(bd9)(fn(chart12)(gen_hyphen_pos(7, 6)));
+        to_hyphen_equal(bd9)(fn(chart12)(pos7_hyphen_6));
         chart13 = g.generate_hyphen_chart2({
           board: bd10,
           source: gen_hyphen_pos(4, 0),
